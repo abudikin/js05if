8f1023dd2efae08bc31022358477b108b4d40414
DENISSP3KTR <54879762+DENISSP3KTR@users.noreply.github.com>
2019-10-29 10:09:49 +0900

    Update if-x3.js

--- if-x3.js
+++ if-x3.js
@@ -1,32 +1,25 @@
 //TODO: Впишите имя функции из задания вместо ...
 //TODO: Добавьте правильные параметры
 //TODO: Напишите функцию
+//11 zadanie
 
 function WeekdayName(n){
 	switch(n){
 		case 1:
-			WeekdayName = 'Понедельник';
-			break;
+			WeekdayName = 'Понедельник'; break;
 		case 2:
-			WeekdayName = 'Вторник';
-			break;
+			WeekdayName = 'Вторник'; break;
 		case 3:
-			WeekdayName = 'Среда';
-			break;
+			WeekdayName = 'Среда'; break;
 		case 4:
-			WeekdayName = 'Четверг';
-			break;
+			WeekdayName = 'Четверг'; break;
 		case 5:
-			WeekdayName = 'Пятница';
-			break;
+			WeekdayName = 'Пятница'; break;
 		case 6:
-			WeekdayName = 'Суббота';
-			break;
+			WeekdayName = 'Суббота'; break;
 		case 7:
-			WeekdayName = 'Воскресенье';
-			break;
-		default:
-			WeekdayName = '';					
+			WeekdayName = 'Воскресенье'; break;
+		default: WeekdayName = '';					
 	}
 	return WeekdayName;
 }
